02c2640a4b33e67044ec244684471602bd568d39
emgreen33 <greenemily33@gmail.com>
2017-01-07 14:48:06 -0600

    Add while true loop

--- easy_bake.py
+++ easy_bake.py
@@ -10,3 +10,11 @@ gpio.setup(38, gpio.IN)
 #true and 1 are the same
 gpio.output(40, True)
 gpio.output(38, 1)
+
+while True:
+  gpio.output(40, True)
+  gpio.output(38, False)
+  sleep(250)
+  gpio.output(40, 0)
+  gpio.output(38, 1)
+
