c057b1424e602d45949fbe63d86fb459d1e0a9ef
emgreen33 <greenemily33@gmail.com>
2017-01-07 19:47:36 -0600

    add stuff

--- pin_lights.py
+++ pin_lights.py
@@ -1,3 +1,32 @@
+# import RPi.GPIO as gpio
+# import time
+
+# #use board numbering on the pi
+
+# gpio.setmode(gpio.BOARD)
+# # output_pins = [40, 38]
+# output_pins = 16
+
+# gpio.setup(output_pins, gpio.OUT)
+
+# #true and 1 are the same
+# # gpio.output(40, True)
+# # gpio.output(38, 1)
+
+# while True:
+#   gpio.output(output_pins, (True, False))
+# #   # gpio.output(40, True)
+# #   # gpio.output(38, False)
+#   time.sleep(1)
+# #   # gpio.output(40, False)
+# #   # gpio.output(38, True)
+#   gpio.output(output_pins, (False, True))
+#   time.sleep(1)
+
+#   gpio.cleanup()
+
+
+
 import RPi.GPIO as gpio
 import time
 
@@ -12,18 +41,18 @@ gpio.setup(output_pins, gpio.OUT)
 # gpio.output(40, True)
 # gpio.output(38, 1)
 
-# while True:
-#   gpio.output(output_pins, (True, False))
-#   # gpio.output(40, True)
-#   # gpio.output(38, False)
-#   time.sleep(1)
-#   # gpio.output(40, False)
-#   # gpio.output(38, True)
-#   gpio.output(output_pins, (False, True))
-#   time.sleep(1)
+while True:
+  gpio.output(output_pins, (True, False))
+  # gpio.output(40, True)
+  # gpio.output(38, False)
+  time.sleep(1)
+  # gpio.output(40, False)
+  # gpio.output(38, True)
+  gpio.output(output_pins, (False, True))
+  time.sleep(1)
+
 
 gpio.cleanup()
-
 
 
 
